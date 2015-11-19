fc313d8e2990da78dfa8a15528a61115f99589e1
Morgan Nesbitt <nmorgan1214@gmail.com>
2015-11-19 14:07:20 -0700

    Copy index draggable to detail
    Bug moves footer when moving cards

--- javascript/sortable.js
+++ javascript/sortable.js
@@ -17,4 +17,23 @@ $(function() {
       icon.toggleClass( "ui-icon-minusthick ui-icon-plusthick" );
       icon.closest( ".portlet" ).find( ".portlet-content" ).toggle();
     });
+    /*-------------------- Detail page sample ------------------------ */
+    $( ".detail-column" ).sortable({
+      connectWith: ".detail-column",
+      handle: ".detail-card-header",
+      cancel: ".detail-card-toggle",
+      placeholder: "portlet-placeholder ui-corner-all"
+    });
+
+    $( ".detail-card" )
+      .addClass( "ui-widget ui-widget-content ui-helper-clearfix ui-corner-all" )
+      .find( ".detail-card" )
+        .addClass( "ui-widget-header ui-corner-all" )
+        .prepend( "<span class='ui-icon ui-icon-minusthick detail-card-toggle'></span>");
+
+    $( ".detail-card-toggle" ).click(function() {
+      var icon = $( this );
+      icon.toggleClass( "ui-icon-minusthick ui-icon-plusthick" );
+      icon.closest( ".detail-card" ).find( ".detail-card-content" ).toggle();
+    });
   });
