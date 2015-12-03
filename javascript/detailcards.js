3529ab4e9eccc8276ebbadb0884835d568ea8415
Morgan Nesbitt <nmorgan1214@gmail.com>
2015-12-03 12:45:29 -0700

    Revert "Temporary GitHub Pages ONLY fix"
    This reverts commit 50c2a3d65a874d8cf5ec1a6f5f7facc651c84398.

--- javascript/detailcards.js
+++ javascript/detailcards.js
@@ -71,83 +71,12 @@
 var cardTemplate;
 var modalTemplate;
 var grantName = "NSF Career";
-var jsonInfo = {
-  Grants: [
-    {
-      Grant_Name : "NSF Career",
-      Grant_Columns : [
-        {
-          Column_Name : "To Do",
-          Cards: [
-            {
-              Card_Name: "Cover Sheet",
-              Modal_ID : "Cover_Sheet",
-              Modal_Body: "<h3>Required</h3><ul><li><b>Program Solicitation Number</b></li><li><b>NSF Unit of Consideration</b></li><li><b>Project Title</b></li><li><b>Co-PIs</b></li><li><b>PI eligibility information</b></li></ul>",
-              Document_Link: "http://www.github.com"
-            },
-            {
-              Card_Name: "Project Description",
-              Modal_ID : "Project_Description",
-              Modal_Body: "<h3>Required</h3><ul><li><b>Program Solicitation Number</b></li><li><b>NSF Unit of Consideration</b></li><li><b>Project Title</b></li><li><b>Co-PIs</b></li><li><b>PI eligibility information</b></li></ul>",
-              Document_Link: "http://www.github.com"
-            },
-            {
-              Card_Name: "Budget",
-              Modal_ID : "Budget",
-              Modal_Body: "<h3>Required</h3><ul><li><b>Program Solicitation Number</b></li><li><b>NSF Unit of Consideration</b></li><li><b>Project Title</b></li><li><b>Co-PIs</b></li><li><b>PI eligibility information</b></li></ul>",
-              Document_Link: "http://www.github.com"
-            },
-            {
-              Card_Name: "Budget Justification",
-              Modal_ID : "Budget_Justification",
-              Modal_Body: "<h3>Required</h3><ul><li><b>Program Solicitation Number</b></li><li><b>NSF Unit of Consideration</b></li><li><b>Project Title</b></li><li><b>Co-PIs</b></li><li><b>PI eligibility information</b></li></ul>",
-              Document_Link: "http://www.github.com"
-            }
-          ]
-        },
+var jsonInfo;
 
-        {
-          Column_Name : "In Progress",
-          Cards: [
-            {
-              Card_Name: "References Cited",
-              Modal_ID : "References_Cited",
-              Modal_Body: "<h3>Required</h3><ul><li><b>Program Solicitation Number</b></li><li><b>NSF Unit of Consideration</b></li><li><b>Project Title</b></li><li><b>Co-PIs</b></li><li><b>PI eligibility information</b></li></ul>",
-              Document_Link: "http://www.github.com",
-              Assigned_People : ["Waffles", "Waffles"]
-            },
-            {
-              Card_Name: "Biosketch",
-              Modal_ID : "Biosketch",
-              Modal_Body: "<h3>Required</h3><ul><li><b>Program Solicitation Number</b></li><li><b>NSF Unit of Consideration</b></li><li><b>Project Title</b></li><li><b>Co-PIs</b></li><li><b>PI eligibility information</b></li></ul>",
-              Document_Link: "http://www.github.com",
-              Assigned_People : ["Waffles"]
-            }
-          ]
-        },
-
-        {
-          Column_Name : "Complete",
-          Cards: [
-            {
-              Card_Name: "Departmental Letter",
-              Modal_ID : "Departmental_Letter",
-              Modal_Body: "<h3>Required</h3><ul><li><b>Program Solicitation Number</b></li><li><b>NSF Unit of Consideration</b></li><li><b>Project Title</b></li><li><b>Co-PIs</b></li><li><b>PI eligibility information</b></li></ul>",
-              Document_Link: "http://www.github.com",
-              Assigned_People : ["Waffles"],
-              Tag_List : ["Under Review", "Complete"]
-            }
-          ]
-        }
-      ]
-    }
-  ]
-};
-/*
 $.getJSON( "json/cards.json", {}, function( data ) {
   jsonInfo = data.Grants[0];
 });
-*/
+
 $.ajax({
     url: 'templates/full_card.html',
     dataType: 'html',
@@ -155,7 +84,7 @@ $.ajax({
     async: false,
     success: function(data) {
         cardTemplate = Handlebars.compile(data);
-        $('#columnList').html(cardTemplate(jsonInfo.Grants[0]));
+        $('#columnList').html(cardTemplate(jsonInfo));
     }
 });
 
@@ -166,6 +95,6 @@ $.ajax({
     async: false,
     success: function(data) {
         modalTemplate = Handlebars.compile(data);
-        $('#cardModals').html(modalTemplate(jsonInfo.Grants[0]));
+        $('#cardModals').html(modalTemplate(jsonInfo));
     }
 });
