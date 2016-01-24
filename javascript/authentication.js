561170a29d069df5740b985eb3292190edd49880
Morgan Nesbitt <nmorgan1214@gmail.com>
2016-01-24 13:20:06 -0700

    Switch to javascript file

---
+++ javascript/authentication.js
@@ -0,0 +1,25 @@
+function onSignIn(googleUser)
+{
+  var profile = googleUser.getBasicProfile();
+  console.log('ID: ' + profile.getId()); // Do not send to your backend! Use an ID token instead.
+  console.log('Name: ' + profile.getName());
+  console.log('Image URL: ' + profile.getImageUrl());
+  console.log('Email: ' + profile.getEmail());
+
+  window.location.href = "overview"
+}
+
+function signOut()
+{
+  var auth2 = gapi.auth2.getAuthInstance();
+  auth2.signOut().then(function () {
+    console.log('User signed out.');
+  });
+  window.location.href = "/"
+}
+
+function onLoad() {
+  gapi.load('auth2', function() {
+    gapi.auth2.init();
+  });
+}
