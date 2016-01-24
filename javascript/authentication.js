9656ce5f9373820d2ffd904b6609b621b04a8312
Morgan Nesbitt <nmorgan1214@gmail.com>
2016-01-24 13:35:47 -0700

    No log in routing

--- javascript/authentication.js
+++ javascript/authentication.js
@@ -1,3 +1,5 @@
+var auth2;
+
 function onSignIn(googleUser)
 {
   var profile = googleUser.getBasicProfile();
@@ -11,15 +13,22 @@ function onSignIn(googleUser)
 
 function signOut()
 {
-  var auth2 = gapi.auth2.getAuthInstance();
+  auth2 = gapi.auth2.getAuthInstance();
   auth2.signOut().then(function () {
     console.log('User signed out.');
+    window.location.href = "/Kanbosal"
   });
-  window.location.href = "/Kanbosal"
 }
 
 function onLoad() {
   gapi.load('auth2', function() {
-    gapi.auth2.init();
+    auth2 = gapi.auth2.init();
+
+    auth2.then(function () {
+      if (!auth2.isSignedIn.get()) {
+        window.location.href="/Kanbosal"
+        console.log("Requested access to page when not logged in")
+      }
+    });
   });
 }
