b7719c2043f5c37bab8a6c4a2f84b60cd0271be1
Allen Nguyen <allendewy@gmail.com>
2016-02-12 11:40:19 -0700

    Changed some styling and created heros, other ui updates.

---
+++ javascript/navbar.js
@@ -0,0 +1,14 @@
+$(function() {
+  var template;
+  var input = {};
+
+  $.ajax({
+    url : "templates/navbar.html",
+    dataType: "html",
+    method: "GET",
+    success: function(data) {
+      template = Handlebars.compile(data);
+      $("nav").append(template(input));
+    }
+  });
+});
