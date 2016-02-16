7768f343906f881d031d6974e3549d523d3be3fb
Morgan Nesbitt <nmorgan1214@gmail.com>
2016-02-16 16:45:13 -0700

    Update signout method redirect

--- javascript/authentication.js
+++ javascript/authentication.js
@@ -15,7 +15,7 @@ function signOut() {
   auth2 = gapi.auth2.getAuthInstance();
   auth2.signOut().then(function () {
     console.log('User signed out.');
-    window.location.href = "/Kanbosal"
+    window.location.href = "/"
   });
 }
 
