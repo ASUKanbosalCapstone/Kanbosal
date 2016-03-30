8ce4dcd8e9dafafd266e6abe8cd99037542d7c01
Lowell Burkett <laburket@asu.edu>
2016-03-30 11:19:44 -0700

    Clean up unused variables, code blocks, and comment blocks.

--- client/javascript/detail.js
+++ client/javascript/detail.js
@@ -1,73 +1,3 @@
-/*
-  Overview: Provided a grant name, will access the respective json file represnting that grant
-    and retrieve the necessary info. There are helper variables in this class to assist in
-    parsing the json file.
-
-  The current structure of the JSON is to be as follows:
-  {
-    "Grants": [
-      {
-        "Grant_Name" : "NSF Career",
-        "Grant_Columns" : [
-          {
-            "Column_Name" : "To Do",
-            "Cards": [
-              {
-                "Card_Name": "Cover Sheet",
-                "Modal_ID" : "Cover_Sheet",
-                "Modal_Body": "<h3>Required</h3><ul><li><b>Program Solicitation Number</b></li><li><b>NSF Unit of Consideration</b></li><li><b>Project Title</b></li><li><b>Co-PIs</b></li><li><b>PI eligibility information</b></li></ul>",
-                "Document_Link": "http://www.github.com"
-              }
-            ]
-          },
-
-          {
-            "Column_Name" : "In Progress",
-            "Cards": [
-              {
-                "Card_Name": "References Cited",
-                "Modal_ID" : "References_Cited",
-                "Modal_Body": "",
-                "Document_Link": "http://www.github.com",
-                "Assigned_People" : ["Waffles", "Waffles"]
-              }
-            ]
-          },
-
-          {
-            "Column_Name" : "Complete",
-            "Cards": [
-              {
-                "Card_Name": "Departmental Letter",
-                "Modal_ID" : "Departmental_Letter",
-                "Modal_Body": "",
-                "Document_Link": "http://www.github.com",
-                "Tag_List" : ["Under Review", "Complete"]
-              }
-            ]
-          }
-        ]
-      }
-    ]
-  }
-
-  NOTE:
-    * Tag_List and Assigned_People are optional
-    * Rest of the fields are required
-
-  Current Status:
-    * Dynamic column generation
-    * Dynamic card generation
-    * Displays body contents on In Progress or Complete column (People, Links, Tags)
-    * Supported Tags for Complete column are, Under Review and Complete
-    * Sick documentation @see above
-  TODO:
-    * Support Comments
-    * Support additional tags
-
-  @author: Morgan Nesbitt
-*/
-
 var totalCards = function(cards) {
   return cards.toDo.length + cards.inProgress.length + cards.complete.length;
 }
@@ -81,7 +11,6 @@ var cardTemplate;
 var modalTemplate;
 var newCardTemplate;
 var cardGenInfo;
-var cardList;
 
 var progressBar = {
   progress: 0
