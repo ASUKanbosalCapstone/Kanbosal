f48b88f807d7ea1c4617d4318edf868359ea6b62
Allen Nguyen <allendewy@gmail.com>
2016-04-09 20:54:08 -0700

    new structure for admin page.

--- client/javascript/administration.js
+++ client/javascript/administration.js
@@ -49,75 +49,22 @@ var initConfirmUser = function (callback) {
 };
 
 var template;
-var templatePend =
-  '<div class="col-xs-4 col-md-3" style="white-space:nowrap;">\
-    <div class="well well-sm">\
-      <img class="img-responsive img-rounded pull-left" style="display:inline-block; margin-right:4px;" src="{{ imageUrl }}">\
-      <a class="btn btn-primary" data-toggle="tooltip" data-placement="top" title="Contact by email." href="mailto:{{ email }}"><i class="fa fa-fw fa-envelope"></i></a>\
-      <span data-toggle="tooltip" data-placement="top" title="Deny user."><button class="btn btn-primary" data-toggle="modal" data-target="#denyUserModal" title="Deny user access." data-userid="{{ _id }}"><i class="fa fa-fw fa-times"></i></button></span>\
-      <span data-toggle="tooltip" data-placement="top" title="Confirm identity."><button class="btn btn-primary" data-toggle="modal" data-target="#confirmUserModal" title="Confirm identity." data-userid="{{ _id }}"><i class="fa fa-fw fa-check"></i></button></span>\
-      <br><strong><small>{{ name }}</small></strong>\
-    </div>\
-  </div>';
-var templateDept =
-  '<div class="list-group-item">\
-    <h5>{{ name }} <small>{{ title }}</small></h5>\
-    <img class="img-responsive img-rounded" style="max-width:45px; display:inline-block;" src="{{ imageUrl }}">\
-    <a class="btn btn-primary btn-lg" href="mailto:{{ email }}" title="Send {{ name }} an email."><i class="fa fa-fw fa-envelope"></i></a>\
-    <a class="btn btn-primary btn-lg" href="#"><i class="fa fa-fw fa-pencil"></i></a>\
-  </div>';
 
 $(function () {
-  $.getJSON('/manageUsers', { 'permissions.stage': -1 }, function (users) {
-    template = Handlebars.compile(templatePend);
-    if (users.length !== 0) {
-      $('#pendingUsersRow').show('fast');
-      for (i in users)
-        $('#pendingUsers > .panel-body > .row').append(template(users[i]));
-    }
-  });
 
-  $.getJSON('/manageUsers', { 'permissions.stage': 0 }, function (users) {
-    template = Handlebars.compile(templateDept);
-    if (users.length !== 0)
-      for (i in users)
-        $('#researchUsers > .list-group').append(template(users[i]));
-    else
-      $('#researchUsers > .panel-body').show('fast');
-  }).then(function () {
-    $('[data-toggle="tooltip"]').tooltip();
-    $('[data-toggle="popover"]').popover({
-      container:'body',
-      html : true
+  $.ajax({
+    url: '/templates/userManager.html',
+    type: 'GET',
+    dataType: 'html',
+  }).done(function(data) {
+    template = Handlebars.compile(data);
+
+    $.getJSON('/getAdmin', function(json) {
+      $('#contentview').append(template(json.pageData));
+      loadNavbar(json.user);
     });
   });
-
-  $.getJSON('/manageUsers', { 'permissions.stage': 1 }, function (users) {
-    template = Handlebars.compile(templateDept);
-    if (users.length !== 0)
-      for (i in users)
-        $('#internalUsers > .list-group').append(template(users[i]));
-    else
-      $('#internalUsers > .panel-body').show('fast');
-  });
-
-  $.getJSON('/manageUsers', { 'permissions.stage': 2 }, function (users) {
-    template = Handlebars.compile(templateDept);
-    if (users.length !== 0)
-      for (i in users)
-        $('#asuUsers > .list-group').append(template(users[i]));
-    else
-      $('#asuUsers > .panel-body').show('fast');
-  });
-
-  $.getJSON('/manageUsers', { 'permissions.stage': -2 }, function (users) {
-    template = Handlebars.compile(templatePend);  // temporarily use pending template
-    if (users.length !== 0)
-      for (i in users)
-        $('#inactiveUsers > .panel-body > .row').append(template(users[i]));
-    else
-      $('#inactiveUsers * .alertEmpty').show('fast');
-  });
+  
 
   $.getJSON('/grants', function(json, textStatus) {
     for (i in json)
