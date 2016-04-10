8a3823dbd462a3810629198d5a51b1ff1265f2bf
Allen Nguyen <allendewy@gmail.com>
2016-04-10 01:52:16 -0700

    admin page ui clean and update. initialize new edit user modal and methods.

--- client/javascript/administration.js
+++ client/javascript/administration.js
@@ -1,11 +1,12 @@
-var denyUser = function (userId) {
+var updateUser = function (userId, jsonObj) {
   $.ajax({
     url: '/users/' + userId,
     type: 'POST',
     contentType: 'application/json',
     data: JSON.stringify({
-      $set: { 'permissions.stage': -2 }
+      $set: jsonObj
     }),
+    async: false,
     success: function() {
       window.location.reload(true);
     },
@@ -13,29 +14,13 @@ var denyUser = function (userId) {
       alert('There was a problem processing your request. Please try again later.');
     }
   });
-};
+}
 
-var confirmUser = function (userId) {
-  initConfirmUser(function (jsonObj) {
-    $.ajax({
-      url: '/users/' + userId,
-      type: 'POST',
-      contentType: 'application/json',
-      data: JSON.stringify({
-        $set: jsonObj
-      }),
-      async: false,
-      success: function() {
-        window.location.reload(true);
-      },
-      error: function () {
-        alert('There was a problem processing your request. Please try again later.');
-      }
-    });
-  });
-};
+var denyUser = function (userId) { updateUser(userId, { 'permissions.stage': -2 }); };
+var confirmUser = function (userId) { initConfirmUser(userId, updateUser); };
+var editUser = function (userId) { initEditUser(userId, updateUser); };
 
-var initConfirmUser = function (callback) {
+var initConfirmUser = function (userId, callback) {
   var jsonObj = { grantIds:[] };
   var formArray = $('#confirmUserForm').serializeArray();
 
@@ -45,28 +30,39 @@ var initConfirmUser = function (callback) {
     else
       jsonObj[formArray[i].name].push(formArray[i].value);
 
-  callback(jsonObj);
+  callback(userId, jsonObj);
+};
+
+var initEditUser = function (userId, callback) {
+  var jsonObj = { grantIds:[] };
+  var formArray = $('#editUserForm').serializeArray();
+
+  for (i in formArray)
+    if (i < 2)
+      jsonObj[formArray[i].name] = parseInt(formArray[i].value, 10);
+    else
+      jsonObj[formArray[i].name].push(formArray[i].value);
+
+  callback(userId, jsonObj);
 };
 
 var template;
 
 $(function () {
-
   $.ajax({
     url: '/templates/userManager.html',
     type: 'GET',
     dataType: 'html',
   }).done(function(data) {
+      console.log('doing stuff');
     template = Handlebars.compile(data);
-
     $.getJSON('/getAdmin', function(json) {
       $('#contentview').append(template(json.pageData));
       loadNavbar(json.user);
     });
   });
-  
 
-  $.getJSON('/grants', function(json, textStatus) {
+  $.getJSON('/grants', function(json) {
     for (i in json)
       $('select#select-grants').append('<option value="' + json[i]._id + '">' + json[i].title + '</option>');
   });
@@ -82,12 +78,21 @@ $(function () {
   $('#confirmUserModal').on('show.bs.modal', function (event) {
     var button = $(event.relatedTarget); // Button that triggered the modal
     var userid = button.data('userid'); // Extract info from data-* attributes
-    var modal = $(this);
-    var form = modal.find('form#confirmUserForm').serializeArray();
 
     $('#confirmUserForm').submit(function (event) {
       event.preventDefault();
       confirmUser(userid);
+      return false;
+    });
+  });
+
+  $('#editUserModal').on('show.bs.modal', function (event) {
+    var button = $(event.relatedTarget); // Button that triggered the modal
+    var userid = button.data('userid'); // Extract info from data-* attributes
+
+    $('#editUserForm').submit(function (event) {
+      event.preventDefault();
+      editUser(userid);
       return false;
     });
   });
