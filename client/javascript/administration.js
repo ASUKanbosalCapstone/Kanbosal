d1c1cf1ad45c59d73a5afaf335b0b294506cc0fc
Allen Nguyen <allendewy@gmail.com>
2016-04-11 10:50:26 -0700

    working, but bugged (in chrome when removing grant from list) edit user form.

--- client/javascript/administration.js
+++ client/javascript/administration.js
@@ -14,7 +14,7 @@ var updateUser = function (userId, jsonObj) {
       alert('There was a problem processing your request. Please try again later.');
     }
   });
-}
+};
 
 var denyUser = function (userId) { updateUser(userId, { 'permissions.stage': -2 }); };
 var confirmUser = function (userId) { initConfirmUser(userId, updateUser); };
@@ -40,21 +40,57 @@ var initEditUser = function (userId, callback) {
   for (i in formArray)
     if (i < 2)
       jsonObj[formArray[i].name] = parseInt(formArray[i].value, 10);
-    else
+    else if (i > 2)
       jsonObj[formArray[i].name].push(formArray[i].value);
 
   callback(userId, jsonObj);
 };
 
+var selectAllGrants = function () {
+  $('#selectedGrantsEdit option').attr('selected', 'selected');
+  console.log('it tried');
+};
+
+var addGrant = function () {
+  var toAdd = $('#selectGrantsEdit').serializeArray();
+  var grantIndex = function (id) {
+    for (i in grants)
+      if (grants[i]._id === id)
+        return i;
+  };
+  
+  if ($('#selectedGrantsEdit').find('option[value="' + toAdd[0].value + '"]').length === 0)
+    $('<option/>', {
+      value: grants[grantIndex(toAdd[0].value)]._id,
+      text: grants[grantIndex(toAdd[0].value)].title
+    }).appendTo('#selectedGrantsEdit');
+
+  $('#selectGrantsEdit').val('');
+};
+
+var removeGrant = function () {
+  var toRemove = $('#selectedGrantsEdit').serializeArray();
+  for (i in toRemove)
+    $('#selectedGrantsEdit option[value="' + toRemove[i].value + '"]').remove();
+};
+
+var cancel = function (modalId) {
+  $(modalId).unbind();
+}
+
 var template;
+var grants;
 
 $(function () {
+  $('[data-toggle="popover"]').popover({
+    html: true
+  });
+
   $.ajax({
     url: '/templates/userManager.html',
     type: 'GET',
     dataType: 'html',
   }).done(function(data) {
-      console.log('doing stuff');
     template = Handlebars.compile(data);
     $.getJSON('/getAdmin', function(json) {
       $('#contentview').append(template(json.pageData));
@@ -63,8 +99,12 @@ $(function () {
   });
 
   $.getJSON('/grants', function(json) {
+    grants = json;
     for (i in json)
-      $('select#select-grants').append('<option value="' + json[i]._id + '">' + json[i].title + '</option>');
+      $('<option/>', {
+        value: json[i]._id,
+        text: json[i].title
+      }).appendTo('#selectGrantsConfirm, #selectGrantsEdit');
   });
 
   $('#denyUserModal').on('show.bs.modal', function (event) {
@@ -78,6 +118,7 @@ $(function () {
   $('#confirmUserModal').on('show.bs.modal', function (event) {
     var button = $(event.relatedTarget); // Button that triggered the modal
     var userid = button.data('userid'); // Extract info from data-* attributes
+    var modal = $(this);
 
     $('#confirmUserForm').submit(function (event) {
       event.preventDefault();
@@ -89,6 +130,27 @@ $(function () {
   $('#editUserModal').on('show.bs.modal', function (event) {
     var button = $(event.relatedTarget); // Button that triggered the modal
     var userid = button.data('userid'); // Extract info from data-* attributes
+    var modal = $(this);
+    var populateGrants = "";
+
+    // populate modal with current user info
+    $.ajax({
+      url: '/users/' + userid,
+      type: 'GET',
+      dataType: 'json'
+    }).done(function(data) {
+      modal.find('#editModalName').html(data.name);
+      modal.find('#stageInputEdit').val(data.permissions.stage);
+      modal.find('#levelInputEdit').val(data.permissions.level);
+      modal.find('#selectedGrantsEdit').empty();
+    }).then(function (data) {
+      for (i in grants)
+        if ($.inArray(grants[i]._id, data.grantIds) !== -1)
+          $('<option/>', {
+            value: grants[i]._id,
+            text: grants[i].title
+          }).appendTo('#selectedGrantsEdit');
+    });
 
     $('#editUserForm').submit(function (event) {
       event.preventDefault();
